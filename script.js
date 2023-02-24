d1fd131e790c0ae084b2fa538967ed56ec2c117e
Balajiw920 <wbalaji2021@gmail.com>
2023-02-24 21:32:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-//your code here
 function minCost(event) {
-	event.preventDefault();
-	var input = document.querySelector("input").value;
-	var arr = input.split(",");
-	var cost=0;
-	while(arr.length > 1){
-		arr.sort(function (a,b) {return a-b});
-		var temp=Number(arr[0]) + Number(arr[1]);
-		cost+=temp;
-		arr.push(temp);
-		arr.splice(0,2);
-		arr.sort(function (a,b) {return a-b});
-		
-	}
-	document.querySelector("div").textContent=cost;
+  event.preventDefault();
+  
+  const input = document.querySelector("input[type=text]");
+  const result = document.getElementById("result");
+  
+  const arr = input.value.split(",").map(Number);
+  
+  const cost = calculateMinCost(arr);
+  
+  result.innerHTML = "Minimum cost: " + cost;
+}
+
+function calculateMinCost(arr) {
+  // Using a priority queue to keep track of the smallest ropes
+  const pq = new MinPriorityQueue();
+  
+  // Insert all ropes into the priority queue
+  for (let i = 0; i < arr.length; i++) {
+    pq.enqueue(arr[i], arr[i]);
+  }
+  
+  let cost = 0;
+  
+  // Repeatedly extract the two smallest ropes, join them, and insert the result back into the priority queue
+  while (pq.size() > 1) {
+    const rope1 = pq.dequeue().element;
+    const rope2 = pq.dequeue().element;
+    const newRope = rope1 + rope2;
+    cost += newRope;
+    pq.enqueue(newRope, newRope);
+  }
+  
+  return cost;
 }
