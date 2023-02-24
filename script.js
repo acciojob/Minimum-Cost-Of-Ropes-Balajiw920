7262379b4f709c36b54c9102fa629f3e1c93a323
Balajiw920 <wbalaji2021@gmail.com>
2023-02-24 20:27:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-function calculateMinCost() {
-  // Get the input value
-  let input_str = document.getElementById("input").value;
-
-  // Convert the input string to a list of integers
-  let arr = input_str.split(",").map(x => parseInt(x.trim()));
-
-  // Initialize a min-heap with the given array
-  let heap = [...arr];
-  heap.sort((a, b) => a - b);
-
-  let cost = 0;
-
-  // Repeat until only one rope remains in the heap
-  while (heap.length > 1) {
-    // Take the two smallest ropes from the heap
-    let smallest1 = heap.shift();
-    let smallest2 = heap.shift();
-
-    // Connect the two ropes and add the cost to the heap
-    let connected = smallest1 + smallest2;
-    cost += connected;
-    heap.push(connected);
-    heap.sort((a, b) => a - b);
-  }
-
-  // Display the result in the div element
-  document.getElementById("result").innerHTML = "Minimum cost: " + cost;
+function minimumCost() {
+    
+    var res = 0;
+    var cost = 0;
+    var inputArray = document.getElementById("inputArr").value;
+    inputArray = inputArray.split(',').map(Number);
+    for (let i = 0; i < inputArray.length - 1; i += 0) {
+        if (inputArray.length == 0) break;
+		// sort by decreasing order
+        inputArray = inputArray.sort((a,b) => b-a);
+        res = inputArray.pop() + inputArray.pop();
+        cost += res;
+		// adding res to arrayy
+        inputArray[inputArray.length] = res;   
+    }  
+    document.getElementById("result").innerHTML = cost;
 }
