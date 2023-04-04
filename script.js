5347f414e51a57965556f82d89dbc8019dfecea0
Balajiw920 <wbalaji2021@gmail.com>
2023-04-05 03:12:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
-function minCost(event) {
-  event.preventDefault();
-  
-  const input = document.querySelector("input[type=text]");
-  const result = document.getElementById("result");
-  
-  const arr = input.value.split(",").map(Number);
-  
-  const cost = calculateMinCost(arr);
-  
-  result.innerHTML = "Minimum cost: " + cost;
-}
+function calculateMinCost() {  
+    var res = 0;  
+      var cost = 0;
+  var inputArray = document.getElementById("rope-lengths").value;
 
-function calculateMinCost(arr) {
-  // Using a priority queue to keep track of the smallest ropes
-  const pq = new MinPriorityQueue();
-  
-  // Insert all ropes into the priority queue
-  for (let i = 0; i < arr.length; i++) {
-    pq.enqueue(arr[i], arr[i]);
-  }
-  
-  let cost = 0;
-  
-  // Repeatedly extract the two smallest ropes, join them, and insert the result back into the priority queue
-  while (pq.size() > 1) {
-    const rope1 = pq.dequeue().element;
-    const rope2 = pq.dequeue().element;
-    const newRope = rope1 + rope2;
-    cost += newRope;
-    pq.enqueue(newRope, newRope);
-  }
-  
-  return cost;
+  inputArray = inputArray.split(',').map(Number);
+  console.log(inputArray);
+  for (let i = 0; i < inputArray.length - 1; i += 0) {
+      if (inputArray.length == 0) break;
+      inputArray = inputArray.sort((a,b) => b-a);
+      res = inputArray.pop() + inputArray.pop();
+      cost += res;
+      inputArray[inputArray.length] = res; }
+  document.getElementById("result").innerHTML = cost;
 }
